ed023774a2c3599106cd66852c31f6c0b66415d9
vujkesu <ivan.vujkovic83@gmail.com>
2022-01-20 00:05:48 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '91d98e9b-b23e-4a0d-85a6-4f3e600cf221';
+export const MODAL_CLOSE_SEC = 2.5;
